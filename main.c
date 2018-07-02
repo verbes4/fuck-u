735d74b7f6f7126a4e63f78c3189a1bec2748a2e
Joshua Hellwege <2xxmlgguyxx2@gmail.com>
2018-07-02 17:09:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,14 +14,18 @@ int main(int argc, char **argv)
     //To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
     //the row and column where you want your cursor to move
     printf("\x1b[16;20HFuck you.");
-
-    while(appletMainLoop())
+    
+	while(appletMainLoop())
     {
         //Scan all the inputs. This should be done once for each frame
         hidScanInput();
 
         //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		if(kDown & KEY_A)
+		{
+			printf(" Shut up.");
+		}
 
         if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
 
