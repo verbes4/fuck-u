4ec0eace63d83fe14d61c974a72c3af038d41548
Joshua Hellwege <2xxmlgguyxx2@gmail.com>
2018-07-09 18:18:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -13,9 +13,8 @@ int main(int argc, char **argv)
     //Move the cursor to row 16 and column 20 and then prints "Hello World!"
     //To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
     //the row and column where you want your cursor to move
-    printf("Heres a handy guide! Not prerssing anything will say 'fuck you', pressing A will say 'Shut up' and pressing B will say 'Bitch'!");
-	printf("\x1b[16;20HFuck you.");
-    
+    printf("Heres a handy guide! Pressing A will say 'Fuck you.', pressing B will say 'Bitch', pressing X will say 'Shut up' and pressing Y will clear the screen (including this guide) ");
+	
 	while(appletMainLoop())
     {
         //Scan all the inputs. This should be done once for each frame
@@ -23,14 +22,25 @@ int main(int argc, char **argv)
 
         //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-		if(kDown & KEY_A)
-		{
-			printf(" Shut up.");
-		}
-
 		if(kDown & KEY_B)
 		{
 			printf(" Bitch.");
+		}
+
+		if(kDown & KEY_X)
+		{
+			printf(" Shut up.");
+		}
+		
+		if(kDown & KEY_Y)
+		{
+		consoleClear();
+
+		}
+		
+		if(kDown & KEY_A)
+		{
+			printf(" Fuck you.");
 		}
 		
         if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
