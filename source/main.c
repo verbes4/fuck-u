9cfebf294c6c7c38713d09737b00672acaae4547
ELY M <elymbmx@gmail.com>
2019-03-02 08:42:10 -0600

    fixed to work on latest libnx

--- source/main.c
+++ source/main.c
@@ -1,14 +1,26 @@
 #include <string.h>
 #include <stdio.h>
-
+#include <stdlib.h>	
 #include <switch.h>
+
+
+/*
+
+HAHAHAHAH this is so funny code :) 
+I fixed it to work with newer linnx :)  
+I would make "Fuck you" text much bigger and add in the middle finger pixel image  
+It can be done by SDL :)
+
+ELY M.  
+
+*/
 
 int main(int argc, char **argv)
 {
-    gfxInitDefault();
 
     //Initialize console. Using NULL as the second argument tells the console library to use the internal console structure as current one.
     consoleInit(NULL);
+	setInitialize();
 
     //Move the cursor to row 16 and column 20 and then prints "Hello World!"
     //To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
@@ -19,9 +31,9 @@ int main(int argc, char **argv)
     {
         //Scan all the inputs. This should be done once for each frame
         hidScanInput();
-
         //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		
 		if(kDown & KEY_B)
 		{
 			printf(" Bitch.");
@@ -40,16 +52,16 @@ int main(int argc, char **argv)
 		
 		if(kDown & KEY_A)
 		{
-			printf(" Fuck you.");
+			printf(" Fuck You.");
 		}
 		
         if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
 		
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gfxWaitForVsync();
+		consoleUpdate(NULL);
+		
+
     }
 
-    gfxExit();
+    consoleExit(NULL);
     return 0;
 }
